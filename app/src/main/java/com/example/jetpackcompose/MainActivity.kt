228e4f3f526b3a392df7aaa6608eacb3d4e49d07
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-01-29 15:52:49 +0300

    Add custom clickable text component and update MainScreen layout

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -16,6 +20,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,8 +42,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(){
-    Text(text="Gülay", modifier = Modifier.padding(5. dp), color = Color.Red, textAlign = TextAlign.Justify)
+    Column(
+        modifier=Modifier.fillMaxSize().background(Color.LightGray),
+        verticalArrangement =  Arrangement.Top
+
+    ){
+        Text(text="Gülay", modifier = Modifier.padding(5. dp), color = Color.Red, textAlign = TextAlign.Justify, fontSize = 30.sp)
+        Spacer(modifier = Modifier.padding(50. dp))
+
+        Text(text="Gülay", modifier = Modifier.padding(5. dp), color = Color.Red, textAlign = TextAlign.Justify, fontSize = 30.sp)
+
+        OzellistirilmisText(text = "hshhshshsh")
+    }
+
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/com/example/jetpackcompose/OzellestirilmisText.kt
+++ app/src/main/java/com/example/jetpackcompose/OzellestirilmisText.kt
@@ -1,4 +1,25 @@
 package com.example.jetpackcompose
 
-class OzellestirilmisText {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun OzellistirilmisText(text:String){
+
+    Text(modifier = Modifier.clickable {
+        println("text tıklandı")
+    }.background(color= Color.White).fillMaxWidth(),text=text)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OzelPreview(){
+    OzellistirilmisText(text="Gülayyyyy Sen İşe Girdin Kanki")
 }
